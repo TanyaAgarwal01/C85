cdd82d57be698930a3392384e637d89096fade9f
TanyaAgarwal01 <tanyaagr2009@gmail.com>
2023-07-12 12:09:35 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB029_0IVpVsUARkdSNZ_NdS96iLJfmpi8",
+  authDomain: "story-telling-app-58e07.firebaseapp.com",
+  databaseURL: "https://story-telling-app-58e07-default-rtdb.firebaseio.com",
+  projectId: "story-telling-app-58e07",
+  storageBucket: "story-telling-app-58e07.appspot.com",
+  messagingSenderId: "32248154050",
+  appId: "1:32248154050:web:8aa48c87329e1cbf70f85b"
+};
